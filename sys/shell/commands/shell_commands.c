2dc66bb7d4ecd43fd7c60d9172fb39e8d04e7ca6
Koen Zandberg <koen@bergzand.net>
2021-04-21 13:32:17 +0200

    shell_commands: Add bpf key value store command

--- sys/shell/commands/shell_commands.c
+++ sys/shell/commands/shell_commands.c
@@ -203,6 +203,10 @@ extern int _cryptoauth(int argc, char **argv);
 extern int _bootloader_handler(int argc, char **argv);
 #endif
 
+#ifdef MODULE_BPF
+extern int _sc_bpf(int argc, char **argv);
+#endif
+
 const shell_command_t _shell_command_list[] = {
     {"reboot", "Reboot the node", _reboot_handler},
     {"version", "Prints current RIOT_VERSION", _version_handler},
@@ -342,6 +346,7 @@ const shell_command_t _shell_command_list[] = {
 #ifdef MODULE_DFPLAYER
     {"dfplayer", "Control a DFPlayer Mini MP3 player", _sc_dfplayer},
 #endif
+<<<<<<< HEAD
 #ifdef MODULE_CONGURE_TEST
     { "cong_clear", "Clears CongURE state object", congure_test_clear_state },
     { "cong_setup", "Calls the setup function for the CongURE state object",
@@ -360,6 +365,9 @@ const shell_command_t _shell_command_list[] = {
       congure_test_msgs_reset },
     { "cong_report", "Calls a report_* method of the CongURE state object",
       congure_test_call_report },
+#endif
+#ifdef MODULE_BPF
+    {"bpf_keyval", "retrieve bpf key value store values", _sc_bpf},
 #endif
     {NULL, NULL, NULL}
 };
