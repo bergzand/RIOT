ac06ff09383696b322d87c54ea704cf2b4ac145a
Koen Zandberg <koen@bergzand.net>
2022-08-31 09:19:26 +0200

    coreconf/hardware: Initial hardware module for coreconf

---
+++ sys/net/coreconf/hardware.c
@@ -0,0 +1,188 @@
+/*
+ * Copyright (C) 2022, Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     net_coreconf
+ * @{
+ * @file
+ * @brief       CORECONF IETF-hardware module implementation
+ *
+ * @author      Koen Zandberg <koen@bergzand.net>
+ */
+
+#include "net/coreconf.h"
+#include "net/gcoap.h"
+#include "xfa.h"
+#include "saul.h"
+#include "saul_reg.h"
+
+#define HARDWARE_MODULE_SID                             1001000
+#define HARDWARE_HARDWARE_SID                           HARDWARE_MODULE_SID + 4
+#define HARDWARE_HARDWARE_COMPONENT_SID                 HARDWARE_MODULE_SID + 14
+#define HARDWARE_HARDWARE_COMPONENT_CLASS_SID           HARDWARE_MODULE_SID + 17
+#define HARDWARE_HARDWARE_COMPONENT_NAME_SID            HARDWARE_MODULE_SID + 26
+#define HARDWARE_HARDWARE_COMPONENT_SD_SID              HARDWARE_MODULE_SID + 30
+#define HARDWARE_HARDWARE_COMPONENT_SD_UNITS_SID        HARDWARE_MODULE_SID + 32
+#define HARDWARE_HARDWARE_COMPONENT_SD_VAL_SID          HARDWARE_MODULE_SID + 33
+#define HARDWARE_HARDWARE_COMPONENT_SD_VAL_SCALE_SID    HARDWARE_MODULE_SID + 35
+#define HARDWARE_HARDWARE_COMPONENT_SD_VAL_TYPE_SID     HARDWARE_MODULE_SID + 37
+
+typedef enum {
+    HW_SENSOR_TYPE_OTHER = 1,
+    HW_SENSOR_TYPE_UNKNOWN = 2,
+    HW_SENSOR_TYPE_VOLTS_AC = 3,
+    HW_SENSOR_TYPE_VOLTS_DC = 4,
+    HW_SENSOR_TYPE_AMPS = 5,
+    HW_SENSOR_TYPE_WATT = 6,
+    HW_SENSOR_TYPE_HERTZ = 7,
+    HW_SENSOR_TYPE_CELSIUS = 8,
+    HW_SENSOR_TYPE_PERCENT_RH = 9,
+    HW_SENSOR_TYPE_RPM = 10,
+    HW_SENSOR_TYPE_CMM = 11,
+    HW_SENSOR_TYPE_TRUTH = 12,
+} hw_sensor_value_type;
+
+static unsigned _phydat_sense2yang_type(uint8_t type)
+{
+    switch (type) {
+        case UNIT_UNDEF:
+            return HW_SENSOR_TYPE_UNKNOWN;
+        case UNIT_V:
+            return HW_SENSOR_TYPE_VOLTS_DC;
+        case UNIT_A:
+            return HW_SENSOR_TYPE_AMPS;
+        case UNIT_W:
+            return HW_SENSOR_TYPE_WATT;
+        case UNIT_TEMP_C:
+            return HW_SENSOR_TYPE_CELSIUS;
+        case UNIT_M3:
+            return HW_SENSOR_TYPE_CMM;
+        case UNIT_BOOL:
+            return HW_SENSOR_TYPE_TRUTH;
+        default:
+            return HW_SENSOR_TYPE_OTHER;
+    }
+}
+
+static size_t _num_saul_devs(void)
+{
+    size_t num = 0;
+    saul_reg_t *dev = saul_reg;
+    while (dev) {
+        dev = dev->next;
+        num++;
+    }
+    return num;
+}
+
+static const char *_devname(const saul_reg_t *dev) {
+    if (dev->name == NULL) {
+        return "(no name)";
+    } else {
+        return dev->name;
+    }
+}
+
+static void _fmt_hw_hardware_component_class(coreconf_encoder_t *enc, const coreconf_node_t *node, const saul_reg_t *dev)
+{
+    (void)dev;
+    (void)node;
+    nanocbor_fmt_uint(coreconf_encoder_cbor(enc), 1000012); /* Temp number for "sensor" */
+}
+
+static void _fmt_hw_hardware_component_name(coreconf_encoder_t *enc, const coreconf_node_t *node, const saul_reg_t *dev)
+{
+    (void)node;
+    nanocbor_put_tstr(coreconf_encoder_cbor(enc), _devname(dev));
+}
+
+static int _fmt_hw_hardware_component_sensor(coreconf_encoder_t *enc, const coreconf_node_t *node, const saul_reg_t *dev)
+{
+    (void)node;
+    int dim;
+    phydat_t res;
+
+    dim = saul_reg_read((saul_reg_t*)dev, &res);
+
+    if (dim < 0) {
+        return -1;
+    }
+
+    nanocbor_fmt_map(coreconf_encoder_cbor(enc), 4);
+
+    coreconf_cbor_sid(enc, HARDWARE_HARDWARE_COMPONENT_SD_SID,
+            HARDWARE_HARDWARE_COMPONENT_SD_UNITS_SID);
+    nanocbor_put_tstr(coreconf_encoder_cbor(enc), phydat_unit_to_str(res.unit));
+
+    coreconf_cbor_sid(enc, HARDWARE_HARDWARE_COMPONENT_SD_SID,
+            HARDWARE_HARDWARE_COMPONENT_SD_VAL_SID);
+    nanocbor_fmt_int(coreconf_encoder_cbor(enc), res.val[0]);
+
+    coreconf_cbor_sid(enc, HARDWARE_HARDWARE_COMPONENT_SD_SID,
+            HARDWARE_HARDWARE_COMPONENT_SD_VAL_SCALE_SID);
+    nanocbor_fmt_int(coreconf_encoder_cbor(enc), res.scale);
+
+    coreconf_cbor_sid(enc, HARDWARE_HARDWARE_COMPONENT_SD_SID,
+            HARDWARE_HARDWARE_COMPONENT_SD_VAL_TYPE_SID);
+    nanocbor_fmt_int(coreconf_encoder_cbor(enc), _phydat_sense2yang_type(res.unit));
+    return 0;
+}
+
+static int _fmt_hw_hardware_component_inst(coreconf_encoder_t *enc, const coreconf_node_t *node, const saul_reg_t *dev)
+{
+    nanocbor_fmt_map(coreconf_encoder_cbor(enc), 3);
+
+    coreconf_cbor_sid(enc, HARDWARE_HARDWARE_COMPONENT_SID,
+            HARDWARE_HARDWARE_COMPONENT_CLASS_SID);
+    _fmt_hw_hardware_component_class(enc, node, dev);
+
+    coreconf_cbor_sid(enc, HARDWARE_HARDWARE_COMPONENT_SID,
+            HARDWARE_HARDWARE_COMPONENT_NAME_SID);
+    _fmt_hw_hardware_component_name(enc, node, dev);
+
+    unsigned type = dev->driver->type;
+
+    if (type & SAUL_CAT_SENSE) {
+        /* It's a sensor */
+        coreconf_cbor_sid(enc, HARDWARE_HARDWARE_COMPONENT_SID,
+                HARDWARE_HARDWARE_COMPONENT_SD_SID);
+        return _fmt_hw_hardware_component_sensor(enc, node, dev);
+    }
+    else {
+        /* It's an actuator */
+    }
+    return 0;
+}
+
+static int _fmt_hw_hardware_component(coreconf_encoder_t *enc, const coreconf_node_t *node)
+{
+    size_t num_devices = _num_saul_devs();
+    nanocbor_fmt_array(coreconf_encoder_cbor(enc), num_devices);
+
+    saul_reg_t *dev = saul_reg;
+    while (dev) {
+        int res = _fmt_hw_hardware_component_inst(enc, node, dev);
+        if (res < 0) {
+            return res;
+        }
+        dev = dev->next;
+    }
+    return 0;
+}
+
+static int _hw_hardware(coreconf_encoder_t *enc, const coreconf_node_t *node)
+{
+    (void)node;
+    nanocbor_fmt_map(coreconf_encoder_cbor(enc), 1);
+    coreconf_cbor_sid(enc, HARDWARE_HARDWARE_SID,
+            HARDWARE_HARDWARE_COMPONENT_SID);
+
+    return _fmt_hw_hardware_component(enc, node);
+}
+
+CORECONF_NODE(HARDWARE_HARDWARE_SID, COAP_GET, _hw_hardware, NULL);
