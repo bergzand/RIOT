492ca639bc524584a13b72f8cb3588c8cd7b5ead
Koen Zandberg <koen@bergzand.net>
2022-09-23 14:22:11 +0200

    system: refactor

--- sys/net/coreconf/system.c
+++ sys/net/coreconf/system.c
@@ -22,39 +22,26 @@
 
 ssize_t coreconf_render_sid(coap_pkt_t *pkt, uint8_t *buf, size_t len, uint64_t sid);
 
-static int _system_platform_node(coreconf_encoder_t *enc, const coreconf_node_t *node)
-{
-    (void)enc;
-    (void)node;
-
-    nanocbor_fmt_map(coreconf_encoder_cbor(enc), 4);
-
-
-    coreconf_fmt_sid(enc, 1724, 1725);
-    coreconf_fmt_sid(enc, 1724, 1726);
-    coreconf_fmt_sid(enc, 1724, 1727);
-    coreconf_fmt_sid(enc, 1724, 1728);
-
-    return 0;
-}
-
-static int _system_platform_machine_node(coreconf_encoder_t *enc, const coreconf_node_t *node)
+static int _system_platform_machine_node(coreconf_encoder_t *enc, const coreconf_node_t *node, void **argv)
 {
     (void)node;
+    (void)argv;
     nanocbor_put_tstr(coreconf_encoder_cbor(enc), RIOT_CPU);
     return 0;
 }
 
-static int _system_platform_osname_node(coreconf_encoder_t *enc, const coreconf_node_t *node)
+static int _system_platform_osname_node(coreconf_encoder_t *enc, const coreconf_node_t *node, void **argv)
 {
     (void)node;
+    (void)argv;
     nanocbor_put_tstr(coreconf_encoder_cbor(enc), "RIOT");
     return 0;
 }
 
-static int _system_platform_osrelease_node(coreconf_encoder_t *enc, const coreconf_node_t *node)
+static int _system_platform_osrelease_node(coreconf_encoder_t *enc, const coreconf_node_t *node, void **argv)
 {
     (void)node;
+    (void)argv;
     uint16_t major = (RIOT_VERSION_CODE >> 48) & 0xFFFF;
     uint16_t minor = (RIOT_VERSION_CODE >> 32) & 0xFFFF;
     uint16_t patch = (RIOT_VERSION_CODE >> 16) & 0xFFFF;
@@ -78,15 +65,16 @@ static int _system_platform_osrelease_node(coreconf_encoder_t *enc, const coreco
     return 0;
 }
 
-static int _system_platform_osversion_node(coreconf_encoder_t *enc, const coreconf_node_t *node)
+static int _system_platform_osversion_node(coreconf_encoder_t *enc, const coreconf_node_t *node, void **argv)
 {
     (void)node;
+    (void)argv;
     nanocbor_put_tstr(coreconf_encoder_cbor(enc), RIOT_VERSION);
     return 0;
 }
 
-CORECONF_NODE(1724, COAP_GET, _system_platform_node, NULL);
-CORECONF_NODE(1725, COAP_GET, _system_platform_machine_node, NULL);
-CORECONF_NODE(1726, COAP_GET, _system_platform_osname_node, NULL);
-CORECONF_NODE(1727, COAP_GET, _system_platform_osrelease_node, NULL);
-CORECONF_NODE(1728, COAP_GET, _system_platform_osversion_node, NULL);
+CORECONF_CONTAINER(root, system_platform, 1724, CORECONF_NODE_STATE);
+CORECONF_LEAF(system_platform, 1725, CORECONF_NODE_STATE, _system_platform_machine_node, NULL);
+CORECONF_LEAF(system_platform, 1726, CORECONF_NODE_STATE, _system_platform_osname_node, NULL);
+CORECONF_LEAF(system_platform, 1727, CORECONF_NODE_STATE, _system_platform_osrelease_node, NULL);
+CORECONF_LEAF(system_platform, 1728, CORECONF_NODE_STATE, _system_platform_osversion_node, NULL);
