dbe7800a23b96f44c6b0625b7ec72a7bb5363739
Koen Zandberg <koen@bergzand.net>
2023-08-09 11:53:49 +0200

    fixup! rbpf/syscall: Add gcoap bindings

--- sys/rbpf/syscalls/syscalls.c
+++ sys/rbpf/syscalls/syscalls.c
@@ -29,6 +29,18 @@ rbpf_call_t rbpf_get_external_call(uint32_t num)
             return rbpf_syscall_get_random_uint32;
         case RBPF_SYSCALL_RANDOM_BUF:
             return rbpf_syscall_get_random_bytes;
+        case RBPF_SYSCALL_GCOAP_REQ_INIT:
+            return rbpf_syscall_coap_init_req;
+        case RBPF_SYSCALL_GCOAP_REQ_SEND:
+            return rbpf_syscall_gcoap_req_send;
+        case RBPF_SYSCALL_COAP_OPT_ADD_URI:
+            return rbpf_syscall_coap_opt_add_uri;
+        case RBPF_SYSCALL_COAP_OPT_ADD_FORMAT:
+            return rbpf_syscall_coap_opt_add_format;
+        case RBPF_SYSCALL_COAP_OPT_FINISH:
+            return rbpf_syscall_coap_opt_finish;
+        case RBPF_SYSCALL_COAP_HDR_SET_TYPE:
+            return rbpf_syscall_coap_hdr_set_type;
         default:
             return NULL;
     }
@@ -53,7 +65,22 @@ static int _add_mem_region(void *ctx, const void *mem, size_t len, virt_syscall_
     return -1;
 }
 
+static intptr_t _alloc_obj_handle(void *ctx, size_t len)
+{
+    rbpf_application_t *rbpf = (rbpf_application_t*)ctx;
+    int handle = rbpf_mem_pool_calloc_handle(&rbpf->pool, len, 0);
+    return handle;
+}
+
+static void *_obj_from_handle(void *ctx, intptr_t handle)
+{
+    rbpf_application_t *rbpf = (rbpf_application_t*)ctx;
+    return rbpf_mem_pool_obj_by_handle(&rbpf->pool, handle);
+}
+
 const virt_syscall_driver_t rbpf_syscall_ctx_driver = {
     .check_mem = _check_mem,
     .add_mem_region = _add_mem_region,
+    .alloc_obj_handle = _alloc_obj_handle,
+    .obj_from_handle = _obj_from_handle,
 };
